7fe4c58fb424d48c7dd68b65b978a19343c1b72c
oggyiu <ragbit@gmail.com>
2016-01-30 15:43:56 +0800

    minor change

--- public/player.js
+++ public/player.js
@@ -18,15 +18,15 @@ var Player = function(game, x, y, id) {
     }
     Phaser.Sprite.call( this, game, x, y, imageName );
 
-    this.anchor.setTo( 0.5, 0.5 );
-    game.physics.enable(this, Phaser.Physics.ARCADE);
-    this.SPEED = 100;
-    this.TURN_RATE = 5;
+    // this.anchor.setTo( 0.5, 0.5 );
+    // game.physics.enable(this, Phaser.Physics.ARCADE);
+    // this.SPEED = 100;
+    // this.TURN_RATE = 5;
 
-    this.animations.add('walk', [ 0, 1 ], 5, true);
-    this.play('walk');
+    // this.animations.add('walk', [ 0, 1 ], 5, true);
+    // this.play('walk');
 
-    game.physics.enable(this, Phaser.Physics.ARCADE);
+    // game.physics.enable(this, Phaser.Physics.ARCADE);
 };
 Player.prototype = Object.create(Phaser.Sprite.prototype);
 Player.prototype.constructor = Player;
