0c7cc4befe9efa3497bf1581df45a46e4ffc8fa2
oggyiu <ragbit@gmail.com>
2016-01-31 02:47:36 +0800

    added monster generator

--- public/monsterGenerator.js
+++ public/monsterGenerator.js
@@ -1,11 +1,23 @@
 var MonsterGenerator = function(game) {
     this.game = game;
-    this.startTime = this.game.time.now;
+    this.lastTime = this.startTime = this.game.time.now;
     this.coolDownTime = 1000;
 };
 MonsterGenerator.prototype.constructor = MonsterGenerator;
 MonsterGenerator.prototype.update = function() {
-    if ( this.coolDownTime >= 0 ) {
-
+    if ( this.coolDownTime > 0 ) {
+        var dt = this.game.time.now - this.lastTime;
+        this.coolDownTime -= dt;
+        if ( this.coolDownTime <= 0 ) {
+            this.generateMonsters();
+            this.coolDownTime = 2000;
+        }
     }
+    this.lastTime = this.game.time.now;
 };
+MonsterGenerator.prototype.generateMonsters = function() {
+    this.game.createEnemyCar(this.game, this.game.getNextEnemyId(), this.game.rnd.integerInRange(0, 3), this.game.rnd.integerInRange(100, 200));
+    this.game.createEnemyCar(this.game, this.game.getNextEnemyId(), this.game.rnd.integerInRange(0, 3), this.game.rnd.integerInRange(100, 200));
+    this.game.createEnemyCar(this.game, this.game.getNextEnemyId(), this.game.rnd.integerInRange(0, 3), this.game.rnd.integerInRange(100, 200));
+    this.game.createEnemyCar(this.game, this.game.getNextEnemyId(), this.game.rnd.integerInRange(0, 3), this.game.rnd.integerInRange(100, 200));
+}
