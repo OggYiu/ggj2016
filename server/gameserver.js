d72e770bc11c9906c00c10a691b08f6a0ed226bd
Nightboy <nightboyx@gmail.com>
2016-01-30 15:31:00 +0800

    Update game server logic

--- server/gameserver.js
+++ server/gameserver.js
@@ -10,25 +10,32 @@ var public_path = '../public';
 var filepath = path.join(__dirname, public_path, 'game.html');
 var appPort = 80;
 
+var gameWorld = {};
+
 app.use(express.static('../public'));
 
 app.get('/', function(req, res) {
   fs.readFile(filepath, 'utf8', function(err, text) {
-    text = text.replace("SERVERIP", ip.address() + ":" + appPort);
+    //text = text.replace("SERVERIP", ip.address() + ":" + appPort);
+    text = text.replace("SERVERIP", "127.0.0.1" + ":" + appPort);
     res.send(text);
   });
 });
 
 io.on('connection', function(client) {
   console.log('Connection to client established');
+  client.emit('message', gameWorld);
 
   // Success!  Now listen to messages to be received
-  client.on('message', function(event) {
-    console.log('Received message from client!', event);
+  client.on('message', function(data) {
+    console.log('Received message from client!', data);
+    gameWorld = data;
+    console.log(gameWorld);
+    //io.broadcast.emit('message', gameWorld);
   });
 
   client.on('disconnect', function() {
-    console.log('Server has disconnected');
+    console.log('Client has disconnected');
   });
 });
 
